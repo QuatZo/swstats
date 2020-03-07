0acf109d1099dcb9b743ed6600555071c29ed940
QuatZo <48104274+QuatZo@users.noreply.github.com>
2020-03-07 15:06:12 +0100

    Mobile Style - Part #1

--- swstatisticsweb/settings.py
+++ swstatisticsweb/settings.py
@@ -187,20 +187,3 @@ CACHES = {
 }
 
 CACHE_TTL = 60 * 5 # 5 minute cache
-
-DEBUG_TOOLBAR_PANEL = [
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.settings.SettingsPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-    'debug_toolbar.panels.templates.TemplatesPanel',
-    'debug_toolbar.panels.cache.CachePanel',
-    'debug_toolbar.panels.signals.SignalsPanel',
-    'debug_toolbar.panels.logging.LoggingPanel',
-    'debug_toolbar.panels.redirects.RedirectsPanel',
-    'debug_toolbar.panels.profiling.ProfilingPanel',
-    'cachalot.panels.CachalotPanel',
-]
