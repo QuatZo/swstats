267134c1735618ae0ecbada11c573d8374465fc1
QuatZo <48104274+QuatZo@users.noreply.github.com>
2020-03-08 13:29:26 +0100

    Update settings.py

--- swstatisticsweb/settings.py
+++ swstatisticsweb/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == 'True'
 
-ALLOWED_HOSTS = ['swstats.info', 'localhost', '51.83.129.23']
+ALLOWED_HOSTS = ['www.swstats.info', 'swstats.info', 'localhost']
 
 
 # Application definition
