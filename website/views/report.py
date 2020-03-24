d33129e77af9041bb78f8efc86cc89223a296fc7
QuatZo <48104274+QuatZo@users.noreply.github.com>
2020-03-24 18:07:05 +0100

    Runes in Monster Report
    * Added Runes Primary value Display name to the Monster Report

--- website/views/report.py
+++ website/views/report.py
@@ -13,7 +13,14 @@ from website.exceptions import ProfileDoesNotExist
 # Create your views here.
 class ReportGeneratorViewSet(viewsets.ViewSet):
     def get_monster_info(self, base_monster):
-        monster = Monster.objects.filter(base_monster=base_monster)
+        monsters = Monster.objects.filter(base_monster=base_monster).prefetch_related('runes')
+        runes = list()
+        for monster in monsters:
+            monster_runes = [ None, None, None, None, None, None ]
+            for rune in monster.runes.all():
+                monster_runes[rune.slot - 1] = rune
+            runes.append({'monster': monster, 'runes': monster_runes})
+
         monster_name = base_monster.name.lower().replace(' (', '_').replace(')', '').replace(' ', '')
         filename = monster_name + '_' + time.strftime("%Y%m%d") + '.csv'
         
@@ -37,7 +44,7 @@ class ReportGeneratorViewSet(viewsets.ViewSet):
 
             fusion = MonsterFusion.objects.filter(monster=base_monster)
 
-        return monster, hoh_exist, hoh_date, fusion.exists(), filename
+        return monsters, hoh_exist, hoh_date, fusion.exists(), filename, runes
 
     def create(self, request):
         # upload by using ajax, focused on stuff that Desktop App had
@@ -45,21 +52,19 @@ class ReportGeneratorViewSet(viewsets.ViewSet):
 
         if request.is_ajax():
             data = request.data
-            ########################################
-            # there will be a report generator
             base_monster = MonsterBase.objects.get(id=data)
             
-            monster, hoh_exist, hoh_date, fusion_exist, filename = self.get_monster_info(base_monster)
+            monsters, hoh_exist, hoh_date, fusion_exist, filename, monsters_runes = self.get_monster_info(base_monster)
 
             context = {
                 'base_monster': base_monster,
-                'monsters': monster,
+                'monsters': monsters,
+                'monsters_runes': monsters_runes,
                 'hoh': hoh_exist,
                 'hoh_date': hoh_date,
                 'fusion': fusion_exist,
                 'filename': filename,
             }
-            ########################################
             html = render_to_string('website/report/report_generate.html', context)
             return HttpResponse(html)
 
