eb823e59d5c343a60c69c4c33e72ae4fa55250aa
QuatZo <48104274+QuatZo@users.noreply.github.com>
2020-05-19 10:09:10 +0200

    Logging Hotfix

--- website/functions.py
+++ website/functions.py
@@ -9,6 +9,7 @@ import matplotlib.cm as cm
 import numpy as np
 import pandas as pd
 import traceback
+import json
 from datetime import timedelta
 
 ########################################################## UPLOAD #########################################################
