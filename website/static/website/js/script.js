ff81e76b934e44f7ec161647e5df1710ecce1e59
QuatZo <48104274+QuatZo@users.noreply.github.com>
2020-10-19 23:46:31 +0200

    Sidebar Update

--- website/static/website/js/script.js
+++ website/static/website/js/script.js
@@ -1,52 +1,34 @@
-var toggle_menu = setInterval(function (){
-    if($('#sidebar-upper').length > 0 && localStorage.getItem('toggled') == "true") { 
-        clearInterval(toggle_menu);
-        $('#sidebar-upper').addClass('toggled');
-        $('#sidebar-lower').addClass('toggled');
-    }
-},1)
-
 $(document).ready(function () {
-    setTimeout(function (){
-        clearInterval(toggle_menu);
-    },5000);
-    $('#sidebar-toggler').on('click', function () {
-        $('#sidebar-upper').toggleClass('toggled');
-        $('#sidebar-lower').toggleClass('toggled');
-
-        var classList = document.getElementById('sidebar-upper').className.split(/\s+/);
-        var toggled = false
-        for (var i = 0; i < classList.length; i++) {
-            if (classList[i] === 'toggled') {
-                toggled = true;
-                break;
-            }
-        }
-        localStorage.setItem('toggled', toggled)
-    });
+  $("#sidebar-toggler").on("click", function () {
+    $("#sidebar-upper").toggleClass("toggled");
+    $("#sidebar-lower").toggleClass("toggled");
+  });
 });
- 
+
 // http://jsfiddle.net/bknE4/81/
 function addParam(url, param, value) {
-    var a = document.createElement('a'), regex = /(?:\?|&amp;|&)+([^=]+)(?:=([^&]*))*/g;
-    var match, str = []; 
-    a.href = url;
-    param = encodeURIComponent(param);
-    var exists = false;
-    var existsText = "";
+  var a = document.createElement("a"),
+    regex = /(?:\?|&amp;|&)+([^=]+)(?:=([^&]*))*/g;
+  var match,
+    str = [];
+  a.href = url;
+  param = encodeURIComponent(param);
+  var exists = false;
+  var existsText = "";
 
-    while (match = regex.exec(a.search)){
-        if (param != match[1]) {
-            str.push(match[1] + (match[2] ? "=" + match[2] : ""));
-        }
-        if (param == match[1]) {
-            exists = true;
-            existsText = match[2];
-        }
+  while ((match = regex.exec(a.search))) {
+    if (param != match[1]) {
+      str.push(match[1] + (match[2] ? "=" + match[2] : ""));
     }
-        
-    if(!exists || exists && existsText != encodeURIComponent(value)) str.push(param+(value?"="+ encodeURIComponent(value):""));
-    a.search = str.join("&");
-   
-    document.location.href = a.href;
+    if (param == match[1]) {
+      exists = true;
+      existsText = match[2];
+    }
+  }
+
+  if (!exists || (exists && existsText != encodeURIComponent(value)))
+    str.push(param + (value ? "=" + encodeURIComponent(value) : ""));
+  a.search = str.join("&");
+
+  document.location.href = a.href;
 }
