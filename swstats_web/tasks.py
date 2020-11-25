4671b4a9887d1bf9fd1b279c7715f8f74d6d6613
QuatZo <48104274+QuatZo@users.noreply.github.com>
2020-11-25 17:54:52 +0100

    Rune Tasks

--- swstats_web/tasks.py
+++ swstats_web/tasks.py
@@ -28,14 +28,66 @@ def fetch_runes_data(self, filters):
     # filters here
     # runes.filter(**filters)
 
-    # temp
-    rune_set_names = runes.values('rune_set__name').annotate(count=Count('rune_set__name'))
+    stars = runes.values('stars').annotate(count=Count('stars'))
+    rune_stars = {}
+    for star in stars:
+        s_c = star['stars'] % 10  # Ancient runes
+        if s_c not in rune_stars:
+            rune_stars[s_c] = {
+                'name': s_c,
+                'count': 0,
+            }
+        rune_stars[s_c]['count'] += star['count']
+
+    qualities = runes.values('quality').annotate(count=Count('quality'))
+    qualities_orig = runes.values('quality_original').annotate(
+        count=Count('quality_original'))
+    rune_qualities = {}
+    for q in qualities:
+        q_n = Rune.get_rune_quality(q['quality']).replace('Ancient ', '')
+        if q_n not in rune_qualities:
+            rune_qualities[q_n] = {
+                'name': q_n,
+                'count': 0,
+                'original': 0,
+            }
+        rune_qualities[q_n]['count'] += q['count']
+    for q in qualities_orig:
+        q_n = Rune.get_rune_quality(
+            q['quality_original']).replace('Ancient ', '')
+        if q_n not in rune_qualities:
+            rune_qualities[q_n] = {
+                'name': q_n,
+                'count': 0,
+                'original': 0,
+            }
+        rune_qualities[q_n]['original'] += q['count']
+
+    primary = runes.values('primary').annotate(count=Count('primary'))
+    rune_primaries = {}
+    for p in primary:
+        p_n = Rune.get_rune_primary(p['primary'])
+        rune_primaries[p_n] = {
+            'name': p_n,
+            'count': p['count'],
+        }
 
     content = {
         'rune_set': [{
             'name': rune_set['rune_set__name'],
             'count': rune_set['count'],
-        } for rune_set in rune_set_names]
+        } for rune_set in runes.values('rune_set__name').annotate(count=Count('rune_set__name'))],
+        'rune_slot': [{
+            'name': rune_slot['slot'],
+            'count': rune_slot['count'],
+        } for rune_slot in runes.values('slot').annotate(count=Count('slot'))],
+        'rune_level': [{
+            'name': rune_level['upgrade_curr'],
+            'count': rune_level['count'],
+        } for rune_level in runes.values('upgrade_curr').annotate(count=Count('upgrade_curr'))],
+        'rune_stars': list(rune_stars.values()),
+        'rune_qualities': list(rune_qualities.values()),
+        'rune_primaries': list(rune_primaries.values()),
     }
 
     return content
